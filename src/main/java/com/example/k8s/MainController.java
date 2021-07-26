6e94e352249acb6b9eb07a057c0c7ee1c1013b3c
Otis <phu.xv97@gmail.com>
2021-07-26 10:26:57 +0700

    first commit

---
+++ src/main/java/com/example/k8s/MainController.java
@@ -0,0 +1,13 @@
+package com.example.k8s;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/home")
+    public String homePage(){
+        return "index";
+    }
+
+}
